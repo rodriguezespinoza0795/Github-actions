35831a9c03814f0fc778d5069e447edbf79d8df2
David Rodriguez <62265080+rodriguezespinoza0795@users.noreply.github.com>
2022-01-05 09:10:35 -0600

    fix bug

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'https://rodriguezespinoza0795.github.io/',
+  base:'https://github.com/rodriguezespinoza0795/Github-actions/',
   plugins: [react()]
 })
